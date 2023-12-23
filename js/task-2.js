4aac71519aabb45e33c1ac2cb5bb9d20881536ab
MykhailoVobolis <mvobolis@gmail.com>
2023-12-23 20:12:25 +0200

    Version 1.4

--- js/task-2.js
+++ js/task-2.js
@@ -201,19 +201,19 @@
 // якщо введено пароль "Я головний", то вивести "Вітаю!",
 //     у іншому випадку виводити рядок "Невірний пароль!".
 
-const userImput = prompt(`Введіть логін`);
-if (userImput === 'Адмін') {
-  const userPassword = prompt(`Введіть пароль`);
-  if (userPassword === 'Я головний') {
-    console.log('Вітаю!');
-  } else {
-    console.log('Невірний пароль!');
-  }
-} else if (!userImput) {
-  console.log('Відмінено');
-} else {
-  console.log('Я вас не знаю!');
-}
+// const userImput = prompt(`Введіть логін`);
+// if (userImput === 'Адмін') {
+//   const userPassword = prompt(`Введіть пароль`);
+//   if (userPassword === 'Я головний') {
+//     console.log('Вітаю!');
+//   } else {
+//     console.log('Невірний пароль!');
+//   }
+// } else if (!userImput) {
+//   console.log('Відмінено');
+// } else {
+//   console.log('Я вас не знаю!');
+// }
 
 //Дано рядок, який складається із символів.Наприклад, 'abcde'.
 //     Перевірте, що першим символом цього рядка є літера 'a'.
@@ -232,3 +232,204 @@ if (userImput === 'Адмін') {
 
 // console.log(name(string, '10'));
 // console.log(name(string2, 'c'));
+
+// const apartment = {
+//   imgUrl: 'https://via.placeholder.com/640x480',
+//   descr: 'Spacious apartment in the city center',
+//   rating: 4.7,
+//   price: 5000,
+//   tags: ['premium', 'promoted', 'top', 'trusted'],
+//   owner: {
+//     name: 'Henry Sibola',
+//     phone: '982-126-1588',
+//     email: 'henry.carter@aptmail.com',
+//   },
+// };
+
+// apartment.area = 60;
+// apartment.rooms = 3;
+// apartment.location = {};
+// apartment.location.country = 'Jamaica';
+// apartment.location.city = 'Kingston';
+
+// console.log(apartment.area);
+// console.log(apartment.rooms);
+// console.log(apartment.location.country);
+// console.log(apartment.location.city);
+
+// const book = {
+//   title: 'The Last Kingdom',
+//   author: 'Bernard Cornwell',
+//   genres: ['historical prose', 'adventure'],
+//   rating: 8.38,
+// };
+
+// for (const key in book) {
+//   console.log(key); // Ключ
+//   console.log(book[key]); // Значення властивості з таким ключем
+// }
+
+// const book = {
+//   author: 'Bernard Cornwell',
+//   genres: ['historical prose', 'adventure'],
+//   rating: 8.38,
+// };
+// const keys = Object.keys(book);
+
+// for (const key of keys) {
+//   console.log(key); // Ключ
+//   console.log(book[key]); // Значення властивості
+// }
+
+// const apartment = {
+//   descr: 'Spacious apartment in the city center',
+//   rating: 4,
+//   price: 2153,
+// };
+
+// const values = [];
+// let keys = apartment;
+
+// keys = Object.keys(keys);
+
+// for (const key of keys) {
+//   values.push(apartment[key]);
+// }
+
+// console.log(values);
+// console.log(keys);
+
+// function countProps(object) {
+//   let propCount = 0;
+
+//   for (const key in object) {
+//     if (object.hasOwnProperty(key)) {
+//       propCount += 1;
+//     }
+//   }
+
+//   return propCount;
+// }
+// console.log(countProps({}));
+// console.log(countProps({ name: 'Mango', age: 2 }));
+// console.log(countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 }));
+
+// function countProps(object) {
+//   const propCount = Object.keys(object).length;
+//   return propCount;
+// }
+// console.log(countProps({}));
+// console.log(countProps({ name: 'Mango', age: 2 }));
+// console.log(countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 }));
+
+// function countTotalSalary(salaries) {
+//   let totalSalary = 0;
+//   const arrayValues = Object.values(salaries);
+//   for (let i of arrayValues) {
+//     totalSalary += i;
+//   }
+//   return totalSalary;
+// }
+
+// console.log(countTotalSalary({}));
+// console.log(countTotalSalary({ mango: 100, poly: 150, alfred: 80 }));
+// console.log(countTotalSalary({ kiwi: 200, poly: 50, ajax: 150 }));
+
+// const colors = [
+//   { hex: '#f44336', rgb: '244,67,54' },
+//   { hex: '#2196f3', rgb: '33,150,243' },
+//   { hex: '#4caf50', rgb: '76,175,80' },
+//   { hex: '#ffeb3b', rgb: '255,235,59' },
+// ];
+
+// const hexColors = [];
+// const rgbColors = [];
+
+// for (let i of colors) {
+//   hexColors.push(i.hex);
+//   rgbColors.push(i.rgb);
+// }
+// console.log(hexColors);
+// console.log(rgbColors);
+
+// function getProductPrice(productName) {
+//   const products = [
+//     { name: 'Radar', price: 1300, quantity: 4 },
+//     { name: 'Scanner', price: 2700, quantity: 3 },
+//     { name: 'Droid', price: 400, quantity: 7 },
+//     { name: 'Grip', price: 1200, quantity: 9 },
+//   ];
+
+//   for (let i of products) {
+//     if (i.name === productName) {
+//       return i.price;
+//     }
+//   }
+//   return null;
+// }
+
+// console.log(getProductPrice('Radar'));
+// console.log(getProductPrice('Grip'));
+// console.log(getProductPrice('Scanner'));
+// console.log(getProductPrice('Droid'));
+// console.log(getProductPrice('Engine'));
+
+// Интересная задача
+// Напиши функцію getAllPropValues(propName), яка приймає один параметр propName - ім'я (ключ) властивості. Функція повинна повернути масив усіх значень властивості з таким ім'ям з кожного об'єкта в масиві products. Якщо в об'єктах відсутні властивості з таким ім'ям, функція повинна повернути порожній масив.
+
+// function getAllPropValues(propName) {
+//   const products = [
+//     { name: 'Radar', price: 1300, quantity: 4 },
+//     { name: 'Scanner', price: 2700, quantity: 3 },
+//     { name: 'Droid', price: 400, quantity: 7 },
+//     { name: 'Grip', price: 1200, quantity: 9 },
+//   ];
+
+//   const values = [];
+//   for (let i of products) {
+//     if (typeof i[propName] !== 'undefined') {
+//       values.push(i[propName]);
+//     } else {
+//       return values;
+//     }
+//   }
+
+//   return values;
+// }
+
+// console.log(getAllPropValues('name'));
+// console.log(getAllPropValues('quantity'));
+// console.log(getAllPropValues('price'));
+// console.log(getAllPropValues('category'));
+
+// Функція calculateTotalPrice(productName) приймає один параметр productName- назва товару. Функція містить масив об'єктів products з такими властивостями, як name — ім'я товару, price — ціна і quantity — кількість.
+
+// Доповни код функції так, щоб вона повертала загальну вартість (ціна * кількість) товару з таким ім'ям з масиву products.
+
+// Якщо продукту з такою назвою немає, то функція повинна повертати рядок "Product <productName> not found!" , де <productName> — це ім'я товару.
+
+function calculateTotalPrice(productName) {
+  const products = [
+    { name: 'Radar', price: 1300, quantity: 4 },
+    { name: 'Scanner', price: 2700, quantity: 3 },
+    { name: 'Droid', price: 400, quantity: 7 },
+    { name: 'Grip', price: 1200, quantity: 9 },
+  ];
+
+  let total;
+  for (let i of products) {
+    if (Object.values(i).includes(productName)) {
+      total = i.price * i.quantity;
+      return total;
+    } else {
+      total = `Product ${productName} not found!`;
+    }
+  }
+  return total;
+}
+
+console.log(calculateTotalPrice('Blaster'));
+console.log(calculateTotalPrice('Radar'));
+console.log(calculateTotalPrice('Droid'));
+console.log(calculateTotalPrice('Grip'));
+console.log(calculateTotalPrice('Scanner'));
