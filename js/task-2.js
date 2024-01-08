331af28305b7cb27404826d493e4a7e5efd1ace3
MykhailoVobolis <mvobolis@gmail.com>
2024-01-08 15:21:19 +0200

    Version 1.8

--- js/task-2.js
+++ js/task-2.js
@@ -2,53 +2,286 @@
 
 'use strict';
 
-const pizzaPalace = {
-  pizzas: ['Supercheese', 'Smoked', 'Four meats'],
-  checkPizza(pizzaName) {
-    return this.pizzas.includes(pizzaName);
-  },
-  order(pizzaName) {
-    const isPizzaAvailable = this.checkPizza(pizzaName);
+// const pizzaPalace = {
+//   pizzas: ['Supercheese', 'Smoked', 'Four meats'],
+//   checkPizza(pizzaName) {
+//     return this.pizzas.includes(pizzaName);
+//   },
+//   order(pizzaName) {
+//     const isPizzaAvailable = this.checkPizza(pizzaName);
 
-    if (!isPizzaAvailable) {
-      return `Sorry, there is no pizza named «${pizzaName}»`;
-    }
+//     if (!isPizzaAvailable) {
+//       return `Sorry, there is no pizza named «${pizzaName}»`;
+//     }
 
-    return `Order accepted, preparing «${pizzaName}» pizza`;
-  },
-};
+//     return `Order accepted, preparing «${pizzaName}» pizza`;
+//   },
+// };
 
-console.log(pizzaPalace.order('Smoked'));
-console.log(pizzaPalace.order('Four meats'));
-console.log(pizzaPalace.order('Big Mike'));
-console.log(pizzaPalace.order('Viennese'));
+// console.log(pizzaPalace.order('Smoked'));
+// console.log(pizzaPalace.order('Four meats'));
+// console.log(pizzaPalace.order('Big Mike'));
+// console.log(pizzaPalace.order('Viennese'));
 
-function foo() {
-  console.log(this);
+// function foo() {
+//   console.log(this);
+// }
+
+// foo();
+
+// const user = {
+//   username: 'Poly',
+//   showThis() {
+//     console.log(this);
+//   },
+// };
+
+// user.showThis();
+
+// function showThis() {
+//   console.log('this in showThis: ', this);
+// }
+
+// // Викликаємо у глобальному контексті
+// showThis();
+
+// const book = {
+//   updateAuthor(newAuthor) {
+//     // ...
+//   },
+// };
+
+// book.updateAuthor('Jacob');
+
+// КЛАСИ
+
+// class User {
+//   constructor(name, email) {
+//     // Ініціалізація властивостей екземпляра
+//     this.name = name;
+//     this.email = email;
+//   }
+// }
+
+// // console.log(User);
+// const mango = new User('Mango', 'mango@mail.com');
+// console.log(mango);
+
+// const poly = new User('Poly', 'poly@mail.com');
+// console.log(poly);
+
+// ПЕРЕДАЧА ОБ'ЄКЕТА ПАРАМЕТРІВ КЛАСУ
+
+// class User {
+//   name;
+//   #email;
+
+//   constructor({ name, email }) {
+//     this.name = name;
+//     this.#email = email;
+//   }
+//   // Публічний метод для отримання електронної пошти
+//   getEmail() {
+//     return this.#email;
+//   }
+
+//   // Публічний метод для зміни електронної пошти
+//   changeEmail(newEmail) {
+//     if (this.#validateEmail(newEmail)) {
+//       this.#email = newEmail;
+//     } else {
+//       console.log('Invalid email format');
+//     }
+//   }
+
+//   // Приватний метод для валідації електронної пошти
+//   #validateEmail(email) {
+//     return email.includes('@');
+//   }
+// }
+
+// const mango = new User({
+//   name: 'Mango',
+//   email: 'mango@mail.com',
+// });
+
+// mango.changeEmail('newmail.com');
+// mango.changeEmail('new@mail.com');
+// console.log(mango.getEmail());
+
+// console.log(User.prototype);
+
+// ГЕТЕРИ І СЕТЕРИ
+
+// class User {
+//   #email;
+
+//   constructor({ name, email }) {
+//     this.name = name;
+//     this.#email = email;
+//   }
+
+//   // Геттер email
+//   get email() {
+//     return this.#email;
+//   }
+
+//   // Сеттер email
+//   set email(newEmail) {
+//     if (newEmail === '') {
+//       console.log('Помилка! Пошта не може бути порожнім рядком!');
+//       return;
+//     }
+//     this.#email = newEmail;
+//   }
+// }
+
+// const mango = new User({
+//   name: 'Mango',
+//   email: 'mango@mail.com',
+// });
+
+// console.log(mango.email);
+// mango.email = 'mango@supermail.com';
+// console.log(mango.email);
+
+// СТАТИЧНІ ВЛАСТИВОСТІ
+
+// class User {
+//   static roles = {
+//     admin: 'admin',
+//     editor: 'editor',
+//     basic: 'basic',
+//   };
+
+//   #email;
+//   #role;
+
+//   constructor({ email, role = User.roles.basic }) {
+//     this.#email = email;
+//     this.#role = role;
+//   }
+
+//   get role() {
+//     return this.#role;
+//   }
+
+//   set role(newRole) {
+//     this.#role = newRole;
+//   }
+// }
+
+// const mango = new User({
+//   email: 'mango@mail.com',
+//   role: User.roles.admin,
+// });
+
+// console.log(mango.role); // "admin"
+// mango.role = User.roles.editor;
+// console.log(mango.role); // "editor"
+
+// СТАТИЧНІ МЕТОДИ
+
+// class User {
+//   static #takenEmails = [];
+
+//   static isEmailTaken(email) {
+//     return User.#takenEmails.includes(email);
+//   }
+
+//   #email;
+
+//   constructor({ email }) {
+//     this.#email = email;
+//     User.#takenEmails.push(email);
+//   }
+// }
+
+// const mango = new User({ email: 'mango@mail.com' });
+
+// console.log(User.isEmailTaken('poly@mail.com')); // false
+// console.log(User.isEmailTaken('mango@mail.com')); // true
+
+// class User {
+//   email;
+
+//   constructor(email) {
+//     this.email = email;
+//   }
+
+//   get email() {
+//     return this.email;
+//   }
+
+//   set email(newEmail) {
+//     this.email = newEmail;
+//   }
+// }
+
+// class Admin extends User {
+//   static role = {
+//     BASIC: 'basic',
+//     SUPERUSER: 'superuser',
+//   };
+//   access;
+//   constructor({ email, access }) {
+//     super(email);
+//   }
+// }
+
+// const mango = new Admin({
+//   email: 'mango@mail.com',
+//   access: Admin.role.SUPERUSER,
+// });
+
+// console.log(mango.email); // "mango@mail.com"
+// console.log(mango.access); // "superuser"
+
+class User {
+  email;
+
+  constructor(email) {
+    this.email = email;
+  }
+
+  get email() {
+    return this.email;
+  }
+
+  set email(newEmail) {
+    this.email = newEmail;
+  }
+}
+class Admin extends User {
+  static role = {
+    BASIC: 'basic',
+    SUPERUSER: 'superuser',
+  };
+  blacklistedEmails = [];
+
+  constructor({ email, access }) {
+    super(email);
+    this.access = access;
+  }
+
+  isBlacklisted(email) {
+    return this.blacklistedEmails.includes(email);
+  }
+
+  blacklist(email) {
+    this.blacklistedEmails.push(email);
+  }
 }
 
-foo();
+const mango = new Admin({
+  email: 'mango@mail.com',
+  access: Admin.role.SUPERUSER,
+});
 
-const user = {
-  username: 'Poly',
-  showThis() {
-    console.log(this);
-  },
-};
+console.log(mango.email); // "mango@mail.com"
+console.log(mango.access); // "superuser"
 
-user.showThis();
-
-function showThis() {
-  console.log('this in showThis: ', this);
-}
-
-// Викликаємо у глобальному контексті
-showThis();
-
-const book = {
-  updateAuthor(newAuthor) {
-    // ...
-  },
-};
-
-book.updateAuthor('Jacob');
+mango.blacklist('poly@mail.com');
+console.log(mango.blacklistedEmails); // ["poly@mail.com"]
+console.log(mango.isBlacklisted('mango@mail.com')); // false
+console.log(mango.isBlacklisted('poly@mail.com')); // true
