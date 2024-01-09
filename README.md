9eab1cafcc227f1a231792fced1acd125ebc685b
MykhailoVobolis <mvobolis@gmail.com>
2024-01-10 00:03:54 +0200

    поиск наибольшего числа массива

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# goit-js-hw-01
+<!-- скрость выполнения кода -->
+
+СТАНДАРТНАЯ ФУНКЦИЯ .toSorted((a, b) => a - b)
+
+Наибольшее число массива: 701 task-2.js:24 Call to doSomething took
+0.19999999925494194 milliseconds.
+
+БИНАРНЫЙ ПОИСК
+
+task-2.js:39 Наибольшее число массива: 701 task-2.js:47 Call to doSomething took
+0.10000000055879354 milliseconds.
--- js/task-2.js
+++ js/task-2.js
@@ -2,286 +2,64 @@
 
 'use strict';
 
-// const pizzaPalace = {
-//   pizzas: ['Supercheese', 'Smoked', 'Four meats'],
-//   checkPizza(pizzaName) {
-//     return this.pizzas.includes(pizzaName);
-//   },
-//   order(pizzaName) {
-//     const isPizzaAvailable = this.checkPizza(pizzaName);
+// создание массива
 
-//     if (!isPizzaAvailable) {
-//       return `Sorry, there is no pizza named «${pizzaName}»`;
-//     }
-
-//     return `Order accepted, preparing «${pizzaName}» pizza`;
-//   },
-// };
-
-// console.log(pizzaPalace.order('Smoked'));
-// console.log(pizzaPalace.order('Four meats'));
-// console.log(pizzaPalace.order('Big Mike'));
-// console.log(pizzaPalace.order('Viennese'));
-
-// function foo() {
-//   console.log(this);
-// }
-
-// foo();
-
-// const user = {
-//   username: 'Poly',
-//   showThis() {
-//     console.log(this);
-//   },
-// };
-
-// user.showThis();
-
-// function showThis() {
-//   console.log('this in showThis: ', this);
-// }
-
-// // Викликаємо у глобальному контексті
-// showThis();
-
-// const book = {
-//   updateAuthor(newAuthor) {
-//     // ...
-//   },
-// };
-
-// book.updateAuthor('Jacob');
-
-// КЛАСИ
-
-// class User {
-//   constructor(name, email) {
-//     // Ініціалізація властивостей екземпляра
-//     this.name = name;
-//     this.email = email;
-//   }
-// }
-
-// // console.log(User);
-// const mango = new User('Mango', 'mango@mail.com');
-// console.log(mango);
-
-// const poly = new User('Poly', 'poly@mail.com');
-// console.log(poly);
-
-// ПЕРЕДАЧА ОБ'ЄКЕТА ПАРАМЕТРІВ КЛАСУ
-
-// class User {
-//   name;
-//   #email;
-
-//   constructor({ name, email }) {
-//     this.name = name;
-//     this.#email = email;
-//   }
-//   // Публічний метод для отримання електронної пошти
-//   getEmail() {
-//     return this.#email;
-//   }
-
-//   // Публічний метод для зміни електронної пошти
-//   changeEmail(newEmail) {
-//     if (this.#validateEmail(newEmail)) {
-//       this.#email = newEmail;
-//     } else {
-//       console.log('Invalid email format');
-//     }
-//   }
-
-//   // Приватний метод для валідації електронної пошти
-//   #validateEmail(email) {
-//     return email.includes('@');
-//   }
-// }
-
-// const mango = new User({
-//   name: 'Mango',
-//   email: 'mango@mail.com',
-// });
-
-// mango.changeEmail('newmail.com');
-// mango.changeEmail('new@mail.com');
-// console.log(mango.getEmail());
-
-// console.log(User.prototype);
-
-// ГЕТЕРИ І СЕТЕРИ
-
-// class User {
-//   #email;
-
-//   constructor({ name, email }) {
-//     this.name = name;
-//     this.#email = email;
-//   }
-
-//   // Геттер email
-//   get email() {
-//     return this.#email;
-//   }
-
-//   // Сеттер email
-//   set email(newEmail) {
-//     if (newEmail === '') {
-//       console.log('Помилка! Пошта не може бути порожнім рядком!');
-//       return;
-//     }
-//     this.#email = newEmail;
-//   }
-// }
-
-// const mango = new User({
-//   name: 'Mango',
-//   email: 'mango@mail.com',
-// });
-
-// console.log(mango.email);
-// mango.email = 'mango@supermail.com';
-// console.log(mango.email);
-
-// СТАТИЧНІ ВЛАСТИВОСТІ
-
-// class User {
-//   static roles = {
-//     admin: 'admin',
-//     editor: 'editor',
-//     basic: 'basic',
-//   };
-
-//   #email;
-//   #role;
-
-//   constructor({ email, role = User.roles.basic }) {
-//     this.#email = email;
-//     this.#role = role;
-//   }
-
-//   get role() {
-//     return this.#role;
-//   }
-
-//   set role(newRole) {
-//     this.#role = newRole;
-//   }
-// }
-
-// const mango = new User({
-//   email: 'mango@mail.com',
-//   role: User.roles.admin,
-// });
-
-// console.log(mango.role); // "admin"
-// mango.role = User.roles.editor;
-// console.log(mango.role); // "editor"
-
-// СТАТИЧНІ МЕТОДИ
-
-// class User {
-//   static #takenEmails = [];
-
-//   static isEmailTaken(email) {
-//     return User.#takenEmails.includes(email);
-//   }
-
-//   #email;
-
-//   constructor({ email }) {
-//     this.#email = email;
-//     User.#takenEmails.push(email);
-//   }
-// }
-
-// const mango = new User({ email: 'mango@mail.com' });
-
-// console.log(User.isEmailTaken('poly@mail.com')); // false
-// console.log(User.isEmailTaken('mango@mail.com')); // true
-
-// class User {
-//   email;
-
-//   constructor(email) {
-//     this.email = email;
-//   }
-
-//   get email() {
-//     return this.email;
-//   }
-
-//   set email(newEmail) {
-//     this.email = newEmail;
-//   }
-// }
-
-// class Admin extends User {
-//   static role = {
-//     BASIC: 'basic',
-//     SUPERUSER: 'superuser',
-//   };
-//   access;
-//   constructor({ email, access }) {
-//     super(email);
-//   }
-// }
-
-// const mango = new Admin({
-//   email: 'mango@mail.com',
-//   access: Admin.role.SUPERUSER,
-// });
-
-// console.log(mango.email); // "mango@mail.com"
-// console.log(mango.access); // "superuser"
-
-class User {
-  email;
-
-  constructor(email) {
-    this.email = email;
+function calculateArray(x) {
+  let testArray = [];
+  for (let index = 0; index < 100000; index++) {
+    let element = Math.round(Math.random() * 100);
+    testArray.push(element);
   }
-
-  get email() {
-    return this.email;
-  }
-
-  set email(newEmail) {
-    this.email = newEmail;
-  }
+  return testArray;
 }
-class Admin extends User {
-  static role = {
-    BASIC: 'basic',
-    SUPERUSER: 'superuser',
-  };
-  blacklistedEmails = [];
+console.log(calculateArray());
 
-  constructor({ email, access }) {
-    super(email);
-    this.access = access;
+// НАИБОЛИШЕЕ ЧИСЛО МАССИВА
+
+const t3 = performance.now();
+
+const maxArrayElement = numberArray => {
+  const sortedNumberArray = numberArray.toSorted((a, b) => a - b);
+  return `Наибольшее число массива: ${
+    sortedNumberArray[sortedNumberArray.length - 1]
+  }`;
+};
+
+// console.log(maxArrayElement());
+
+console.log(
+  maxArrayElement([
+    19, 48, 34, 20, 5, 10, 489, 499, 38, 47, 598, 192, 109, 701, 498, 93, 93,
+    399, 192, 499, 92, 0, 325, 532, 183, 621, 184, 619, 172, 398, 308, 298, 698,
+    700, 583, 568, 342, 432,
+  ])
+);
+
+const t4 = performance.now();
+console.log('Call to doSomething took ' + (t4 - t3) + ' milliseconds.');
+
+// / БИНАРНЫЙ ПОИСК
+
+const t0 = performance.now();
+
+const elementsArray = numbers => {
+  let newNumberArray = numbers;
+  while (newNumberArray.length >= 2) {
+    let numberNew = newNumberArray[Math.round(newNumberArray.length / 2)];
+    newNumberArray = newNumberArray.filter(element => element > numberNew);
   }
+  return `Наибольшее число массива: ${newNumberArray}`;
+};
 
-  isBlacklisted(email) {
-    return this.blacklistedEmails.includes(email);
-  }
+// console.log(elementsArray());
 
-  blacklist(email) {
-    this.blacklistedEmails.push(email);
-  }
-}
+console.log(
+  elementsArray([
+    19, 48, 34, 20, 5, 10, 489, 499, 38, 47, 598, 192, 109, 701, 498, 93, 93,
+    399, 192, 499, 92, 0, 325, 532, 183, 621, 184, 619, 172, 398, 308, 298, 698,
+    700, 583, 568, 342, 432,
+  ])
+);
 
-const mango = new Admin({
-  email: 'mango@mail.com',
-  access: Admin.role.SUPERUSER,
-});
-
-console.log(mango.email); // "mango@mail.com"
-console.log(mango.access); // "superuser"
-
-mango.blacklist('poly@mail.com');
-console.log(mango.blacklistedEmails); // ["poly@mail.com"]
-console.log(mango.isBlacklisted('mango@mail.com')); // false
-console.log(mango.isBlacklisted('poly@mail.com')); // true
+const t1 = performance.now();
+console.log('Call to doSomething took ' + (t1 - t0) + ' milliseconds.');
