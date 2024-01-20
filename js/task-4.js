0076897985378910fb685ba14acd0534ba8dae60
MykhailoVobolis <mvobolis@gmail.com>
2024-01-20 11:18:13 +0200

    Пример ООП / классы

--- js/task-4.js
+++ js/task-4.js
@@ -48,4 +48,52 @@ function seasonDefinition(month) {
     }
   }
 }
-console.log(seasonDefinition('Январь'));
+console.log(seasonDefinition('Октябрь'));
+
+// Завершите функцию, которая принимает два аргумента и возвращает все числа, которые делятся на данный делитель. Первый аргумент - это массив чисел, а второй - делитель.
+
+// function learnJavaScript() {
+//   const divisibleBy = (numbers, divisor) => {
+//     return numbers.filter(num => num % divisor === 0);
+//   };
+//   return divisibleBy([1, 2, 3, 4, 5, 6], 2);
+// }
+
+// console.log(learnJavaScript());
+
+//Ваша функция принимает два аргумента:
+
+// текущий возраст отца (лет)
+// текущий возраст сына (лет)
+// Подсчитайте, сколько лет назад отец был вдвое старше сына (или через сколько лет он будет вдвое старше).
+
+function learnJavaScript() {
+  const twiceAsOld = (dadYearsOld, sonYearsOld) => {
+    return Math.abs(sonYearsOld * 2 - dadYearsOld);
+  };
+  return twiceAsOld(81, 40);
+}
+
+console.log(learnJavaScript());
+
+// ООП классы и элементы классов
+
+class User {
+  constructor(firsname, lastname, raceteam) {
+    this.firsname = firsname;
+    this.lastname = lastname;
+    this.raceteam = raceteam;
+  }
+
+  leaderChampionship() {
+    return `Formula 1 race championship leader ${this.firsname} ${this.lastname} from the ${this.raceteam} team`;
+  }
+}
+
+const user1 = new User('Max', 'Verstappen', 'RedBull');
+const user2 = new User('Fernando', 'Alonso', 'Aston Martin');
+const user3 = new User('Lando', 'Norris', 'Mclaren');
+
+console.log(user1.leaderChampionship());
+console.log(user2.leaderChampionship());
+console.log(user3.leaderChampionship());
